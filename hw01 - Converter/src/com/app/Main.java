adf73597be4cde5092e08fd0a32ba48402955d89
sparrowchirp <tetianapavliuchenko@gmail.com>
2025-12-15 17:02:29 +0100

    Updated app info

--- hw01 - Converter/src/com/app/Main.java
+++ hw01 - Converter/src/com/app/Main.java
@@ -2,7 +2,7 @@ package com.app;
 
 public class Main {
      public static void main(String[] args) {
-        System.out.println("My app");
+        System.out.println("My git app for users");
     }
 }
 
