17e030a07a9d9ee69b15ff3f3408811cf6ca6530
sparrowchirp <tetianapavliuchenko@gmail.com>
2025-12-15 17:27:46 +0100

    Change app info

--- hw01 - Converter/src/com/app/Main.java
+++ hw01 - Converter/src/com/app/Main.java
@@ -2,7 +2,7 @@ package com.app;
 
 public class Main {
      public static void main(String[] args) {
-        System.out.println("My git app for everybody");
+        System.out.println("My app for everybody");
     }
 }
 
