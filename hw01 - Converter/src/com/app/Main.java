0138c68690ef6a1f17c1e0c1a35679de5517b1d5
sparrowchirp <tetianapavliuchenko@gmail.com>
2025-12-15 17:11:28 +0100

    Add multiplication

--- hw01 - Converter/src/com/app/Main.java
+++ hw01 - Converter/src/com/app/Main.java
@@ -2,7 +2,22 @@ package com.app;
 
 public class Main {
      public static void main(String[] args) {
-        System.out.println("My git app for users");
+         System.out.println("My git app. Super!");
+
+         int a = 8;
+         int b = 2;
+         int c = add(a, b);
+         System.out.println(c);
+         int d = multiply(a, b);
+         System.out.println(d);
+     }
+
+    private static int add(int a, int b) {
+        return a + b;
+    }
+
+    private static int multiply(int a, int b) {
+        return a * b;
     }
 }
 
