d2cf50ec05f8e189039bf4f1227cc9519578558d
sparrowchirp <tetianapavliuchenko@gmail.com>
2025-12-17 00:05:00 +0100

    Added converter 2.0

--- HomeWorks/src/com/app/hw02/Converter.java
+++
@@ -1,7 +0,0 @@
-package com.app.hw02;
-
-public class Converter {
-    static void main() {
-        System.out.println("Converter version 2.0");
-    }
-}
---
+++ HomeWorks/src/com/app/hw02/ConverterKmMiles.java
@@ -0,0 +1,24 @@
+package com.app.hw02;
+
+public class ConverterKmMiles {
+
+    private static final double CONV = 0.621371192;
+
+    static void main() {
+        System.out.println("Converter version 2.0");
+
+        double miles = 10;
+        double km = 5;
+
+        System.out.println(miles + " miles is " + convMilesToKgs(miles));
+        System.out.println( km + " kilometers is " + convKmToMiles(km));
+    }
+
+    private static double convMilesToKgs(double miles) {
+        return miles / CONV;
+    }
+
+    private static double convKmToMiles(double km) {
+        return km * CONV;
+    }
+}
