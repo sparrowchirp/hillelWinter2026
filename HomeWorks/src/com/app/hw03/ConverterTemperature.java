499bd82502146681e024026351752665d91bf74a
sparrowchirp <tetianapavliuchenko@gmail.com>
2025-12-21 14:28:54 +0100

    Add converting Celsius to Fahrenheit

--- HomeWorks/src/com/app/hw03/ConverterTemperature.java
+++ HomeWorks/src/com/app/hw03/ConverterTemperature.java
@@ -3,5 +3,14 @@ package com.app.hw03;
 public class ConverterTemperature {
     static void main() {
         System.out.println("Converter Temperature");
+
+        double temperatureCelsius = 90;
+        double temperatureFahrenheit = 200;
+
+        System.out.println(temperatureCelsius + "°C is " + convToFahrenheit(temperatureCelsius));
+    }
+
+    private static double convToFahrenheit(double temperatureCelsius) {
+        return (temperatureCelsius * 1.8 + 32);
     }
 }
